5b6bda36cf994c887b744ebddd21b0ccf8c9acef
Prakasa <i450220@fhict.nl>
2022-05-17 15:25:36 +0200

    Editing brand guide content through dashboard

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ function myFunction() {
     const element = document.getElementById("duplicate1");
     element.remove();
   }
+
+  window.addEventListener('load' ,() =>{
+
+    const introh1 = localStorage.getItem('INTROH1');
+    const introp1 = localStorage.getItem('INTROP1');
+
+    document.getElementById('introtext').innerHTML = introh1 ;
+    document.getElementById('intropara').innerHTML = introp1;
+  })
