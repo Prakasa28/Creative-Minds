2939ea5e565b386cffb41da91a36f8e1e11142a0
Prakasa <i450220@fhict.nl>
2022-05-29 23:56:40 +0200

    Menu sortable and image upload

--- main.js
+++ main.js
@@ -1,22 +1,66 @@
+// Duplicate menu item
 var TTi = 0;
 var TToriginal = document.getElementById("duplicate");
 function copy() {
-    var TTclone = TToriginal.cloneNode(true);
-    TTclone.id = "duplicate" + ++TTi;
-    TToriginal.parentNode.appendChild(TTclone);
+  var TTclone = TToriginal.cloneNode(true);
+  TTclone.id = "duplicate" + ++TTi;
+  TToriginal.parentNode.appendChild(TTclone);
+  localStorage.setItem("wasPageRefreshed", true);
+}
+if (localStorage.getItem("wasPageRefreshed")) {
+  copy();
 }
 
-
 function myFunction() {
-    const element = document.getElementById("duplicate1");
-    element.remove();
+  const element = document.getElementById("duplicate1");
+  localStorage.removeItem("wasPageRefreshed");
+  element.remove();
+}
+
+//Change text
+window.addEventListener("load", () => {
+  const introh1 = localStorage.getItem("INTROH1");
+  const introp1 = localStorage.getItem("INTROP1");
+
+  document.getElementById("introtext").innerHTML = introh1;
+  document.getElementById("intropara").innerHTML = introp1;
+});
+
+// upload logo
+document.querySelector("#logoUpload").addEventListener("change", function () {
+  const reader = new FileReader();
+
+  reader.addEventListener("load", () => {
+    localStorage.setItem("logo-image", reader.result);
+  });
+
+  reader.readAsDataURL(this.files[0]);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const logoImageDataUrl = localStorage.getItem("logo-image");
+
+  if (logoImageDataUrl) {
+    document
+      .querySelector("#logoPreview")
+      .setAttribute("src", logoImageDataUrl);
   }
+});
 
-  window.addEventListener('load' ,() =>{
+//Menu sortable
 
-    const introh1 = localStorage.getItem('INTROH1');
-    const introp1 = localStorage.getItem('INTROP1');
+$(function () {
+  $("#sortable").sortable();
+});
 
-    document.getElementById('introtext').innerHTML = introh1 ;
-    document.getElementById('intropara').innerHTML = introp1;
-  })
+function dragdrop() {
+  var list = document.getElementsByClassName("Sidebar_menu")[0].outerHTML;
+  localStorage.setItem("dragdropmenu", list);
+}
+
+/* window.addEventListener('load' ,() =>{
+
+const dragdropmenu = localStorage.getItem('dragdropmenu');
+document.getElementById('sortable').outerHTML = dragdropmenu ;
+
+})*/
